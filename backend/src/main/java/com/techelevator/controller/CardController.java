e769aa98725be5bdc2114d31ef139ab885a5b557
Lindsay Leigh <LindsayJoLeigh@gmail.com>
2021-04-08 10:51:10 -0400

    updates to CardJDBC, controller

--- backend/src/main/java/com/techelevator/controller/CardController.java
+++ backend/src/main/java/com/techelevator/controller/CardController.java
@@ -1,7 +1,10 @@
 package com.techelevator.controller;
 
+import java.security.Principal;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +13,8 @@ import com.techelevator.dao.CardDAO;
 import com.techelevator.model.Card;
 
 @RestController
-@RequestMapping("/cards")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class CardController {
 	
 	private CardDAO dao;
@@ -19,10 +23,23 @@ public class CardController {
 		this.dao = cardDAO;
 	}
 	
-	@RequestMapping(path = "/{userId}", method = RequestMethod.GET)
-	public List<Card> getAllCards(@PathVariable int userId) {
-		return dao.getAllCards(userId);
+	@RequestMapping(path = "/user/cards", method = RequestMethod.GET)
+	public List<Card> getAllCards(Principal principal) {
+		return dao.getAllCards(principal.getName());
 	}
 	
+	@RequestMapping(path = "/user/{cardId}", method = RequestMethod.GET)
+	public Card getCardById(Principal principal, @PathVariable int cardId) {
+		return dao.getCardById(principal.getName(), cardId);
+	}
+	
+	@RequestMapping(path = "/user/{subject}", method = RequestMethod.GET)
+	public List<Card> getCardsBySubject(Principal principal, @PathVariable String subject) {
+		return dao.getCardsBySubject(principal.getName(), subject);
+	}
+	
+	//placeholder for getCardsByKeyword
 
 }
+
+
--- backend/src/main/java/com/techelevator/dao/JDBCCardDAO.java
+++ backend/src/main/java/com/techelevator/dao/JDBCCardDAO.java
@@ -19,13 +19,15 @@ public class JDBCCardDAO implements CardDAO {
 	}
 
 	@Override
-	public List<Card> getAllCards(int userId) {
+	public List<Card> getAllCards(String user) {
 		List<Card> allCards = new ArrayList<>();
 		
-		String sqlGetAllCards = "SELECT question, answer, subject.subject_name FROM cards"
-				+ " JOIN subject USING (subject_id) WHERE creator_id = ?;";
-		
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllCards, userId); 
+		String sqlGetAllCards = "SELECT question, answer, subject.subject_name FROM cards\n" + 
+				"JOIN subject USING (subject_id)\n" + 
+				"JOIN users on users.user_id = cards.creator_id\n" + 
+				"WHERE username = ?;";
+	
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllCards, user); 
 		while (results.next()) {
 			Card card = mapRowToCard(results);
 			allCards.add(card);
@@ -35,42 +37,54 @@ public class JDBCCardDAO implements CardDAO {
 	}
 	
 	@Override
-	public Card getCardById(int userId, int cardId) {
+	public Card getCardById(String user, int cardId) {
 		
 		String sqlGetCardById = "SELECT question, answer, subject.subject_name FROM cards"
-				+ " JOIN subject USING (subject_id) WHERE creator_id = ? AND card_id = ?;";
+				+ " JOIN subject USING (subject_id) JOIN users ON creator_id = user_id"
+				+ " WHERE users.username = ? AND card_id = ?;";
 		
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetCardById, userId, cardId);
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetCardById, user, cardId);
 		results.next();
 		Card card = mapRowToCard(results);
 		
 		return card;
 	}
 
-
-
 	@Override
-	public List<Card> getCardsBySubject(int userId, String subject) {
+	public List<Card> getCardsBySubject(String user, String subject) {
 		
 		List<Card> cards = new ArrayList<>();
 		
-		String sqlGetCardBySubject = "SELECT question, answer, subject.subject_name FROM cards"
-				+ " JOIN subject USING (subject_id) WHERE creator_id = ? AND subject.subject_name = ?;";
+		String sqlGetCardsBySubject = "SELECT question, answer, subject.subject_name FROM cards"
+				+ " JOIN subject USING (subject_id) JOIN users ON creator_id = user_id"
+				+ " WHERE users.username = ? AND subject.subject_name = ?;";
 		
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetCardsBySubject, userId, subject);
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetCardsBySubject, user, subject);
 		while (results.next()) {
 			Card card = mapRowToCard(results);
 			cards.add(card);
-		}
-				 
-				
+		}		 				
 		return cards;
 	}
 
 	@Override
-	public List<Card> getCardsByKeyword(String keyword) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Card> getCardsByKeyword(String user, String keyword) {
+		List<Card> cards = new ArrayList<>();
+		
+		String sqlGetCardsByKeyword = "";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetCardsByKeyword, user, keyword);
+		while (results.next()) {
+			Card card = mapRowToCard(results);
+			cards.add(card);
+		}
+		
+		return cards;
+	}
+	
+	@Override
+	public boolean createCard(String question, String answer, String subject, String[] keywords) {
+		return true;
 	}
 	
 	private Card mapRowToCard(SqlRowSet rs) {
@@ -79,8 +93,7 @@ public class JDBCCardDAO implements CardDAO {
 		card.setAnswer(rs.getString("answer"));
 		card.setSubject(rs.getString("subject_name"));
 		
-		return card;
-	
+		return card;	
 	}
 
 }
