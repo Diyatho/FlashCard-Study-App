f125c7702e085a8e0c1abc63e8549e112bed544c
Diya Loveson <diyatho@gmail.com>
2021-04-08 14:54:47 -0400

    Create a card

--- frontend/src/services/CardService.js
+++ frontend/src/services/CardService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+// The below steps are commented out because the auth token is linked to axios header. This is done in 
+// store-->index.js
 // const http = axios.create({
 //     baseURL: 'http://localhost:8080'
-
 // });
 
 //axios.get("http://localhost:8080/user/cards
@@ -10,7 +10,9 @@ import axios from 'axios';
 export default{
     getCards(){
         return axios.get('/user/cards');
+    },
+    createCard(newCard){
+    return axios.post('/user/cards/create', newCard);
     }
-    
 };
 
