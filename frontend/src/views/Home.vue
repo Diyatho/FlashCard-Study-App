03ee21ba256f5540a06f0c3a78abc9558abc1358
Arwen Eger <arweneger@gmail.com>
2021-04-09 18:30:45 -0400

    updating styling

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
+    <img src="@/assets/TEHOOT.png" alt="logo">
     <h1>Home</h1>
     <p id = "welcome">Welcome {{username}}!!</p>
 
     <decks/>
-
-   <!-- <router-link v-bind:to="{ name: 'createCard' }">Create A Deck</router-link><br/>-->
-    <router-link v-bind:to="{ name: 'createDeck' }">Create A Deck</router-link><br/>
+<div class="form">
+  <form class="form-options">
+     <button router-link v-bind:to="{ name: 'createDeck' }" class=createADeck type="submit">Create A Deck</button><br>
+  
+   <router-link class=createADeck v-bind:to="{ name: 'createDeck' }" tag="button">Create A Deck</router-link>
     <!--<router-link v-bind:to="{ name: 'viewDecks' }">View Saved Decks</router-link><br/>-->
     <router-link v-bind:to="{ name: 'studySession' }">Start a study session</router-link><br/>
     <router-link v-bind:to="{ name: 'viewCards' }">View All Cards</router-link>
-      
+  </form>
+  </div>
+   <br>
+   <img id="Owl" src="@/assets/TEHOOTOwl.png" alt="Owl">    
   </div>
 </template>
 
@@ -40,10 +46,68 @@ export default {
 </script>
 
 <style>
-
-
+img{ 
+  width: 800px;
+}
+#Owl{
+  width: 275px;
+  margin: 15px;
+}
 #welcome{
   font-size: 20px;
+}
+div.form
+{
+    display: block;
+    text-align: center;
+}
+form {
+  font-size: 20px;
+  padding: 15px;
+  background: rgb(211, 211, 211);
+  border-style: solid;
+  max-width: fit-content;
+    display: inline-block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  
+}
+
+.form-control{
+  margin: 10px;
+  padding: 5px;
+}
+
+button{
+  font-size: 20px;
+  margin: 7px;
+}
+.createADeck {
+  -moz-box-shadow:inset 0px -1px 3px 0px #91b8b3;
+	-webkit-box-shadow:inset 0px -1px 3px 0px #91b8b3;
+	box-shadow:inset 0px -1px 3px 0px #91b8b3;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffff00), color-stop(1, #ffff00));
+	background:-moz-linear-gradient(top, #C57BFF 5%, #C57BFF 100%);
+	background:-webkit-linear-gradient(top, #C57BFF 5%, #C57BFF 100%);
+	background:-o-linear-gradient(top, #C57BFF 5%, #C57BFF 100%);
+	background:-ms-linear-gradient(top, #C57BFF 5%, #C57BFF 100%);
+	background:linear-gradient(to bottom, #C57BFF 5%, #C57BFF 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffff00', endColorstr='#ffff00',GradientType=0);
+	background-color:#C57BFF;
+	-webkit-border-radius:5px;
+	-moz-border-radius:5px;
+	border-radius:5px;
+	border:1px solid #566963;
+	display:inline-block;
+	cursor:pointer;
+	color:#00000b;
+	font-family:Arial;
+	font-size:20px;
+	font-weight:bold;
+	padding:11px 23px;
+	text-decoration:none;
+/* text-shadow:0px -1px 0px #2b665e; */
 }
 </style>
 
